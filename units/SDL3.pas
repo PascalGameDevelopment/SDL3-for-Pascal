36d7b240d486de506243e4ed6e75ab51772ddadd
Matthias <info@freepascal-meets-sdl.net>
2026-04-04 01:20:54 +0200

    Updates SDL_pixels.inc to 3.4.2

--- units/SDL3.pas
+++ units/SDL3.pas
@@ -88,7 +88,7 @@ const
 {$I SDL_loadso.inc}                       // 3.2.0
 {$I SDL_rect.inc}                         // 3.4.2
 {$I SDL_properties.inc}                   // 3.4.2
-{$I SDL_pixels.inc}                       // 3.1.6-prev
+{$I SDL_pixels.inc}                       // 3.4.2
 {$I SDL_blendmode.inc}                    // 3.1.6-prev
 {$I SDL_iostream.inc}                     // 3.2.0
 {$I SDL_asyncio.inc}                      // 3.2.0
