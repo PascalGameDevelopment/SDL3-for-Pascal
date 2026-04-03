8faf69f8b80550dee95ec432fdea8bac97be2e7e
Matthias <info@freepascal-meets-sdl.net>
2026-04-03 22:14:53 +0200

    Update SDL_video.inc to 3.4.2

--- units/SDL3.pas
+++ units/SDL3.pas
@@ -93,7 +93,7 @@ const
 {$I SDL_iostream.inc}                     // 3.2.0
 {$I SDL_asyncio.inc}                      // 3.2.0
 {$I SDL_surface.inc}                      // 3.2.20
-{$I SDL_video.inc}                        // 3.2.20
+{$I SDL_video.inc}                        // 3.4.2
 {$I SDL_timer.inc}                        // 3.2.20
 {$I SDL_error.inc}                        // 3.1.6-prev
 {$I SDL_power.inc}                        // 3.1.6-prev
