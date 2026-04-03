4632859e51806d6b59380ce2b11d78165f4ddb04
Matthias <info@freepascal-meets-sdl.net>
2026-04-04 00:41:01 +0200

    Update SDL_revision.inc to 3.4.2

--- units/SDL3.pas
+++ units/SDL3.pas
@@ -78,7 +78,7 @@ const
   SDL_init.inc --> SDL_init.h             this version of the header file: }
 {$I SDL_log.inc}                          // 3.4.2
 {$I SDL_version.inc}                      // 3.4.2
-{$I SDL_revision.inc}                     // 3.1.6-prev
+{$I SDL_revision.inc}                     // 3.4.2
 {$I SDL_locale.inc}                       // 3.2.0
 {$I SDL_guid.inc}                         // 3.1.6-prev
 {$I SDL_hints.inc}                        // 3.2.12
