b314e76f5298d81b8c692612211e2aabf95d5d71
Matthias <info@freepascal-meets-sdl.net>
2026-04-05 01:04:02 +0200

    Update SDL_touch.inc to 3.4.4

--- units/SDL3.pas
+++ units/SDL3.pas
@@ -106,7 +106,7 @@ const
 {$I SDL_joystick.inc}                     // 3.4.4
 {$I SDL_gamepad.inc}                      // 3.2.0
 {$I SDL_haptic.inc}                       // 3.2.0
-{$I SDL_touch.inc}                        // 3.1.6-prev
+{$I SDL_touch.inc}                        // 3.4.4
 {$I SDL_pen.inc}                          // 3.2.20
 {$I SDL_camera.inc}                       // 3.1.6-prev
 {$I SDL_events.inc}                       // 3.2.20
