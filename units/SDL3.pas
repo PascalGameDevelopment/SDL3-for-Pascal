4842c513d62a387993db3ef9fb239fe25ac07077
Matthias <info@freepascal-meets-sdl.net>
2026-04-05 00:51:57 +0200

    Update SDL_joystick.inc to 3.4.4

--- units/SDL3.pas
+++ units/SDL3.pas
@@ -103,7 +103,7 @@ const
 {$I SDL_keycode.inc}                      // 3.2.20
 {$I SDL_mouse.inc}                        // 3.4.4
 {$I SDL_keyboard.inc}                     // 3.4.4
-{$I SDL_joystick.inc}                     // 3.1.6-prev
+{$I SDL_joystick.inc}                     // 3.4.4
 {$I SDL_gamepad.inc}                      // 3.2.0
 {$I SDL_haptic.inc}                       // 3.2.0
 {$I SDL_touch.inc}                        // 3.1.6-prev
