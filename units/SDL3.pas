bcb06a71f47d6dd5a9adb06be088ebae4d5e1b8a
Matthias <info@freepascal-meets-sdl.net>
2026-04-04 23:37:22 +0200

    Update SDL_mouse.inc to 3.4.4

--- units/SDL3.pas
+++ units/SDL3.pas
@@ -101,7 +101,7 @@ const
 {$I SDL_sensor.inc}                       // 3.4.4
 {$I SDL_scancode.inc}                     // 3.4.4
 {$I SDL_keycode.inc}                      // 3.2.20
-{$I SDL_mouse.inc}                        // 3.1.6-prev
+{$I SDL_mouse.inc}                        // 3.4.4
 {$I SDL_keyboard.inc}                     // 3.1.6-prev
 {$I SDL_joystick.inc}                     // 3.1.6-prev
 {$I SDL_gamepad.inc}                      // 3.2.0
