8923d73002fb0d3593681b57d4f580cf6fcaf350
Matthias <info@freepascal-meets-sdl.net>
2026-04-07 10:21:44 +0200

    Update SDL_asyncio.inc to 3.4.4

--- units/SDL3.pas
+++ units/SDL3.pas
@@ -91,7 +91,7 @@ const
 {$I SDL_pixels.inc}                       // 3.4.2
 {$I SDL_blendmode.inc}                    // 3.4.2
 {$I SDL_iostream.inc}                     // 3.4.4
-{$I SDL_asyncio.inc}                      // 3.2.0
+{$I SDL_asyncio.inc}                      // 3.4.4
 {$I SDL_surface.inc}                      // 3.2.20
 {$I SDL_video.inc}                        // 3.4.2
 {$I SDL_timer.inc}                        // 3.2.20
