83859592733def7811a12d4cec4910c01c5d28ad
Matthias <info@freepascal-meets-sdl.net>
2026-04-07 12:09:43 +0200

    Update SDL_messagebox.inc to 3.4.4

--- units/SDL3.pas
+++ units/SDL3.pas
@@ -116,7 +116,7 @@ const
 {$I SDL_clipboard.inc}                    // 3.4.4
 {$I SDL_cpuinfo.inc}                      // 3.4.4
 {$I SDL_dialog.inc}                       // 3.4.4
-{$I SDL_messagebox.inc}                   // 3.2.0
+{$I SDL_messagebox.inc}                   // 3.4.4
 {$I SDL_time.inc}                         // 3.2.0
 {$I SDL_filesystem.inc}                   // 3.2.0
 {$I SDL_atomic.inc}                       // 3.2.0
