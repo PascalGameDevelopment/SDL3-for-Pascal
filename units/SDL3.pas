2d343d615a9c1df8c610369c7b0e8cc827928814
Matthias <info@freepascal-meets-sdl.net>
2026-04-07 12:26:56 +0200

    Update SDL_hidapi.inc to 3.4.4

--- units/SDL3.pas
+++ units/SDL3.pas
@@ -120,7 +120,7 @@ const
 {$I SDL_time.inc}                         // 3.4.4
 {$I SDL_filesystem.inc}                   // 3.4.4
 {$I SDL_atomic.inc}                       // 3.4.4
-{$I SDL_hidapi.inc}                       // 3.2.0
+{$I SDL_hidapi.inc}                       // 3.4.4
 {$I SDL_metal.inc}                        // 3.2.0
 {$I SDL_vulkan.inc}                       // 3.2.0
 {$I SDL_thread.inc}                       // 3.2.0
