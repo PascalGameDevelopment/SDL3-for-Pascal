ab4a80842b684b5519ad77315295fd75f564e838
Matthias <info@freepascal-meets-sdl.net>
2026-04-07 09:51:40 +0200

    Update SDL_loadso.inc version tag

--- units/SDL3.pas
+++ units/SDL3.pas
@@ -85,7 +85,7 @@ const
 {$I SDL_misc.inc}                         // 3.4.4
 {$I SDL_stdinc.inc}                       // 3.1.6-prev (unfinished)
 {$I SDL_platform.inc}                     // 3.4.4
-{$I SDL_loadso.inc}                       // 3.2.0
+{$I SDL_loadso.inc}                       // 3.4.4
 {$I SDL_rect.inc}                         // 3.4.2
 {$I SDL_properties.inc}                   // 3.4.2
 {$I SDL_pixels.inc}                       // 3.4.2
