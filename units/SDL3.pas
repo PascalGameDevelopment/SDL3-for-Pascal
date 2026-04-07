27451cff9dbb1dc5eb547db890d7d90f401043db
Matthias <info@freepascal-meets-sdl.net>
2026-04-07 22:06:22 +0200

    Update SDL_timer.inc to 3.4.4

--- units/SDL3.pas
+++ units/SDL3.pas
@@ -94,7 +94,7 @@ const
 {$I SDL_asyncio.inc}                      // 3.4.4
 {$I SDL_surface.inc}                      // 3.4.4
 {$I SDL_video.inc}                        // 3.4.2
-{$I SDL_timer.inc}                        // 3.2.20
+{$I SDL_timer.inc}                        // 3.4.4
 {$I SDL_error.inc}                        // 3.4.2
 {$I SDL_power.inc}                        // 3.4.2
 {$I SDL_audio.inc}                        // 3.4.2
